5fc63565a8a500d14ae6e0a9741b85b063b8761f
Kevin McDonagh <kevin@novoda.com>
2010-05-29 19:03:55 +0100

    Added the bare bones app and structure.

---
+++ app/src/zen/garden/Garden.java
@@ -0,0 +1,15 @@
+package zen.garden;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Garden extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
