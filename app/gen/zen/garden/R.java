af773279d0dd32895c842acfb2aa0c67babccab6
Kevin McDonagh <kevin@novoda.com>
2010-05-29 20:16:08 +0100

    Added the layout xmls.

--- app/gen/zen/garden/R.java
+++ app/gen/zen/garden/R.java
@@ -14,7 +14,12 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class layout {
-        public static final int dashboard=0x7f030000;
+        public static final int animations=0x7f030000;
+        public static final int controls=0x7f030001;
+        public static final int dashboard=0x7f030002;
+        public static final int dialogs=0x7f030003;
+        public static final int forms=0x7f030004;
+        public static final int lists=0x7f030005;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
